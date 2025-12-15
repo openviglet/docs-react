06eab84495159e22bf0b36b0770bab62cc6ee89c
Alexandre Oliveira <alexandre.oliveira@gmail.com>
2025-12-15 12:32:35 -0300

    Update docusaurus.config.ts

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -26,7 +26,8 @@ const config: Config = {
   organizationName: 'openviglet', // Usually your GitHub org/user name.
   projectName: 'docs-react', // Usually your repo name.
 
-  onBrokenLinks: 'throw',
+  onBrokenLinks: 'warn',
+  onBrokenMarkdownLinks: 'warn',
 
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
